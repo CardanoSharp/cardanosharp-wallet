0e98cdbf939bdb85d199ad2ee5ba731b88539819
Daniel Schaefer <danieljschaefer@outlook.com>
2021-06-03 19:25:24 -0700

    Building Out More Domain things and added todos for tiems started but not yet finished

--- CardanoSharp.Wallet/Models/Transactions/TransactionBody/Certificate/Certificate.cs
+++ CardanoSharp.Wallet/Models/Transactions/TransactionBody/Certificate/Certificate.cs
@@ -4,10 +4,10 @@
     {
         public byte[] StakeRegistration { get; set; }
         public byte[] StakeDeregistration { get; set; }
-        public byte[] StakeDelegation { get; set; }
-        public byte[] PoolRegistration { get; set; }
-        public byte[] PoolRetirement { get; set; }
-        public byte[] GenesisKeyDelegation { get; set; }
-        public byte[] MoveInstantaneousRewardsCert { get; set; }
+        public StakeDelegation StakeDelegation { get; set; }
+        public PoolRegistration PoolRegistration { get; set; }
+        public PoolRetirement PoolRetirement { get; set; } //TODO Finish Type
+        public byte[] GenesisKeyDelegation { get; set; }//TODO Finish Type
+        public byte[] MoveInstantaneousRewardsCert { get; set; }//TODO Finish Type
     }
 }
---
+++ CardanoSharp.Wallet/Models/Transactions/TransactionBody/Certificate/PoolRetirement.cs
@@ -0,0 +1,13 @@
+﻿namespace CardanoSharp.Wallet.Models.Transactions
+{
+    //    pub struct PoolRetirement
+    //    {
+    //        pool_keyhash: Ed25519KeyHash,
+    //    epoch: Epoch,
+    //}
+    public partial class PoolRetirement
+    {
+        public byte[] PoolKeyHash { get; set; }
+        public uint Epoch { get; set; }
+    }
+}
