60d30d62321f13352b2f72d0e92fbf7f532aca61
Daniel-SchaeferJ <danieljschaefer@outlook.com>
2021-06-07 08:04:09 -0700

    Update creation

---
+++ CardanoSharp.Wallet/Models/Transactions/Update.cs
@@ -0,0 +1,11 @@
+﻿namespace CardanoSharp.Wallet.Models.Transactions
+{
+    //update = [proposed_protocol_parameter_updates
+    //     , epoch
+    //     ]
+    public partial class Update
+    {
+        public byte[] ProposedProtocolParameterUpdates { get; set; }
+        public int Epoch { get; set; }
+    }
+}
