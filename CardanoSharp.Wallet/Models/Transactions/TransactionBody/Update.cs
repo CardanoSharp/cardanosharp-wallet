8426d482a35b3d8b6c4f64d540452eb3ad156cca
Daniel-SchaeferJ <danieljschaefer@outlook.com>
2021-06-07 08:10:45 -0700

    Added comment

--- CardanoSharp.Wallet/Models/Transactions/TransactionBody/Update.cs
+++ CardanoSharp.Wallet/Models/Transactions/TransactionBody/Update.cs
@@ -2,6 +2,10 @@
 
 namespace CardanoSharp.Wallet.Models.Transactions
 {
+
+    //update = [proposed_protocol_parameter_updates
+    //     , epoch
+    //     ]
     public partial class Update
     {
         public Dictionary<byte[], object> ProposedProtocolParameterUpdates { get; set; }
