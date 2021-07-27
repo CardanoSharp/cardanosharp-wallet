ee17f0b14ec44becbd98e6781a0baf681a3a0d58
alex klee <alex.klee@outlook.com>
2021-07-28 00:46:47 +0200

    suggestion for address type

--- CardanoSharp.Wallet.Test/AddressTests.cs
+++ CardanoSharp.Wallet.Test/AddressTests.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using CardanoSharp.Wallet.Encoding;
+using CardanoSharp.Wallet.Enums;
+using CardanoSharp.Wallet.Models.Addresses;
+using CardanoSharp.Wallet.Extensions.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,14 +14,17 @@ namespace CardanoSharp.Wallet.Test
     public class AddressTests
     {
         private readonly IAddressService _addressService;
+        private readonly IKeyService _keyService;
+        const string __mnemonic = "art forum devote street sure rather head chuckle guard poverty release quote oak craft enemy";
 
         public AddressTests()
         {
             _addressService = new AddressService();
+            _keyService = new KeyService();
         }
 
         [Theory]
-        //Delegation Addresses
+        //Delegation Addresses    
         [InlineData("addr_test", "addr_test1qz2fxv2umyhttkxyxp8x0dlpdt3k6cwng5pxj3jhsydzer3jcu5d8ps7zex2k2xt3uqxgjqnnj83ws8lhrn648jjxtwq2ytjqp")]
         [InlineData("addr", "addr1qx2fxv2umyhttkxyxp8x0dlpdt3k6cwng5pxj3jhsydzer3jcu5d8ps7zex2k2xt3uqxgjqnnj83ws8lhrn648jjxtwqfjkjv7")]
         [InlineData("addr_test", "addr_test1qpu5vlrf4xkxv2qpwngf6cjhtw542ayty80v8dyr49rf5ewvxwdrt70qlcpeeagscasafhffqsxy36t90ldv06wqrk2qum8x5w")]
@@ -41,5 +48,80 @@ namespace CardanoSharp.Wallet.Test
 
             Assert.Equal(addr, addr2);
         }
+
+        //28 
+        //addr1q8d9pcrn38veygv638ftw0f82gm4h6rmrs599pkr3qfxx073eyjrmj0hnx6xz8emx03l6hszjclm8fmnlaewe4adp7dqsd8pa6
+        //28
+        [Theory]
+        [InlineData("addr", "addr1q8d9pcrn38veygv638ftw0f82gm4h6rmrs599pkr3qfxx073eyjrmj0hnx6xz8emx03l6hszjclm8fmnlaewe4adp7dqsd8pa6")]
+        [InlineData("addr_test", "addr_test1qz2fxv2umyhttkxyxp8x0dlpdt3k6cwng5pxj3jhsydzer3jcu5d8ps7zex2k2xt3uqxgjqnnj83ws8lhrn648jjxtwq2ytjqp")]
+        public void FromStringTest(string prefix, string addr)
+        {
+            var addrByte = _addressService.GetAddressBytes(addr);
+            var address = new Address(addr);
+            var hex = address.ToHexString();
+
+            Assert.Equal(addrByte, address.GetBytes());
+            Assert.Equal(prefix, address.Prefix);
+            Assert.Equal(prefix == "addr" ? NetworkType.Mainnet : NetworkType.Testnet, address.NetworkType);
+            Assert.Equal(AddressType.Base, address.AddressType);
+
+        }
+
+        /// <summary>
+        /// Illustrating the fact that addresses generated with paymentPub & stakePub indeed have multiple parts
+        /// verifying components of adresses 
+        /// inspired by Andrew Westberg (NerdOut) Addresses Video 
+        /// https://www.youtube.com/watch?v=NjPf_b9UQNs&t=396)
+        /// </summary>
+        [Theory]
+        [InlineData(__mnemonic, "cc339a35f9e0fe039cf510c761d4dd29040c48e9657fdac7e9c01d94")]
+        public void VerifyRewardAddress(string mnemonic, string stakingAddr)
+        {
+            // create two payment addresses from same root key
+            //arrange
+            var entropy = _keyService.Restore(mnemonic);
+            var rootKey = _keyService.GetRootKey(entropy);
+                        
+            ////get payment keys
+            (var paymentPrv1, var paymentPub1) = getKeyPairFromPath("m/1852'/1815'/0'/0/0", rootKey);
+            (var paymentPrv2, var paymentPub2) = getKeyPairFromPath("m/1852'/1815'/0'/0/1", rootKey);
+            (var paymentPrv3, var paymentPub3) = getKeyPairFromPath("m/1852'/1815'/0'/0/2", rootKey);
+
+            ////get stake keys
+            (var stakePrv, var stakePub) = getKeyPairFromPath("m/1852'/1815'/0'/2/0", rootKey);
+            
+            var baseAddr1 = _addressService.GetAddress(paymentPub1, stakePub, NetworkType.Testnet, AddressType.Base);
+            var baseAddr2 = _addressService.GetAddress(paymentPub2, stakePub, NetworkType.Testnet, AddressType.Base);
+            var baseAddr3 = _addressService.GetAddress(paymentPub3, stakePub, NetworkType.Testnet, AddressType.Base);
+
+            //act
+            var hex1 = baseAddr1.ToAddress().ToHexString(); // 0 0 79467c69a9ac66280174d09d62575ba955748b21dec3b483a9469a65 cc339a35f9e0fe039cf510c761d4dd29040c48e9657fdac7e9c01d94
+            var hex2 = baseAddr2.ToAddress().ToHexString(); // 0 0 1fd57d18565e3a17cd194f190d349c2b7309eaf70f3f3bf884b0eada cc339a35f9e0fe039cf510c761d4dd29040c48e9657fdac7e9c01d94
+            var hex3 = baseAddr3.ToAddress().ToHexString(); // 0 0 f36b29ceede650f850ee705a3a89ec041e24397d1a0d803d6af7e3f2 cc339a35f9e0fe039cf510c761d4dd29040c48e9657fdac7e9c01d94
+                                                            // ╎ ╎ ╎                                                        ╎
+                                                            // ╎ ╎ ╰╌ Payment Address                                       ╰╌ Reward Address 
+                                                            // ╎ ╰╌╌╌ NetworkType 0 = Testnet
+                                                            // ╰╌╌╌╌╌ AddressType 0 = Base
+            // assert
+            Assert.EndsWith(stakingAddr, hex1);
+            Assert.EndsWith(stakingAddr, hex2);
+            Assert.EndsWith(stakingAddr, hex3);
+
+        }
+
+        /// <summary>
+        /// Getting the key from path as descibed in https://github.com/cardano-foundation/CIPs/blob/master/CIP-1852/CIP-1852.md
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="rootKey"></param>
+        /// <returns></returns>
+        private (byte[], byte[]) getKeyPairFromPath(string path, (byte[], byte[]) rootKey)
+        {
+            var privateKey = _keyService.DerivePath(path, rootKey.Item1, rootKey.Item2);
+            var publicKey = _keyService.GetPublicKey(privateKey.Item1, false);
+            return (privateKey.Item1, publicKey);
+        }
+
     }
 }
---
+++ CardanoSharp.Wallet/Models/Addresses/Address.cs
@@ -0,0 +1,131 @@
+﻿using CardanoSharp.Wallet.Encoding;
+using CardanoSharp.Wallet.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CardanoSharp.Wallet.Models.Addresses
+{
+    public class Address : IEqualityComparer<Address>, IEquatable<Address>, IEquatable<string>, IEquatable<byte[]>
+    {
+        private byte[] _bytes;
+        private string _address;
+        public AddressType AddressType { get; }
+        public NetworkType NetworkType { get; }
+        
+        public Address()
+        {
+        }
+
+        public Address(string prefix, byte[] address)
+        {
+            var bech32 = new Bech32();
+            _bytes = address;
+            _address = bech32.Encode(address, prefix);
+        }
+
+        public Address(string address)
+        {
+            if (string.IsNullOrWhiteSpace(address)) 
+                throw new ArgumentNullException(nameof(address));
+
+            var bech32 = new Bech32();
+            
+            //if (!bech32.HasValidChars(address))
+            //{
+            //    throw new ArgumentException("Invalid characters", nameof(address));
+            //}
+
+            _address = address;
+            _bytes = bech32.Decode(_address, out byte witVer, out string prefix);
+
+            Prefix = prefix;
+            WitnessVersion = witVer;
+            AddressType = GetAddressType();
+            NetworkType = GetNetworkType();
+        }
+
+        /// <summary>
+        /// Returns <see cref="AddressType"/> as defined in https://github.com/cardano-foundation/CIPs/blob/master/CIP-0019/CIP-0019.md
+        /// </summary>
+        /// <returns>
+        ///     <para><see cref="AddressType.Base"/> if AddressType header is 0x01</para>
+        ///     <para><see cref="AddressType.Enterprise"/> if AddressType header is 0x06</para>
+        ///     <para>otherwise <see cref="AddressType.Base"/></para>
+        /// </returns>
+        /// <returns></returns>
+        private AddressType GetAddressType()
+        {
+            return (_bytes[0] >> 4) switch
+            {
+                0x01 => AddressType.Base, //@TODO: derive all AddressTypes
+                0x06 => AddressType.Enterprise,
+                //0x07 => AddressType.SmartContract,
+                _ => AddressType.Base,
+            };
+        }
+
+        /// <summary>
+        /// Returns <see cref="NetworkType"/> as defined in https://github.com/cardano-foundation/CIPs/blob/master/CIP-0019/CIP-0019.md
+        /// </summary>
+        /// <returns>
+        ///     <para><see cref="NetworkType.Testnet"/> if Metwork header is 0x00</para>
+        ///     <para><see cref="NetworkType.Mainnet"/> if Network header is 0x01</para>
+        /// </returns>
+        /// <exception cref="InvalidOperationException">If LSB is </exception>"
+        private NetworkType GetNetworkType()
+        {
+            return (_bytes[0] & 0x0f) switch
+            {
+                0x00 => NetworkType.Testnet,
+                0x01 => NetworkType.Mainnet,
+                _ => throw new InvalidOperationException("unknown network type"),
+            };
+        }
+
+
+        public string Prefix { get; set; }
+        public byte WitnessVersion { get; set; }
+
+        public bool Equals(string other)
+        {
+            return _address.Equals(other);
+        }
+
+        public bool Equals(byte[] other)
+        {
+            return _bytes.Equals(other);
+        }
+
+        public bool Equals(Address other)
+        {
+            if (other == null) return false;
+            return _bytes.Equals(other.GetBytes());
+        }
+
+        public byte[] GetBytes()
+        {
+            return _bytes;
+        }
+
+        public override string ToString()
+        {
+            return _address;
+        }
+
+        public string ToHexString()
+        {
+            return BitConverter.ToString(_bytes).Replace("-","").ToLower();
+        }
+
+        public bool Equals(Address x, Address y)
+        {
+            return x.Equals(y);
+        }
+
+        public int GetHashCode(Address obj)
+        {
+            return obj.GetBytes().GetHashCode();
+        }
+    }
+}
