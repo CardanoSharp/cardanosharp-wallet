9c2c4733b7144abf6e3840dbc340d21282408163
alex klee <alex.klee@outlook.com>
2021-08-01 16:40:24 +0200

    Cleanup tests

--- CardanoSharp.Wallet.Test/HDWallet/DerivationTests.cs
+++ CardanoSharp.Wallet.Test/HDWallet/DerivationTests.cs
@@ -102,33 +102,6 @@ namespace CardanoSharp.Wallet.Test
             if (depth == 5) AssertDerivedKeys(prv, pub, index);
         }
 
-        [Fact]
-        public void ImplicitPathDerivationTest()
-        {
-            var path = "m/1852'/1815'/0'/0/0";
-
-            // Arrange
-            var mnemonic = new KeyService().Restore(__mnemonic);
-            var rootKey = mnemonic.GetRootKey();
-            var testKey = getTestRootKey(mnemonic);
-
-            (var paymentPrv1, var paymentPub1) = getKeyPairFromPath(path, rootKey);
-
-            // Act
-            // Fluent derivation API
-            var derivation = testKey.Derive(RoleType.ExternalChain)      // implicit IRoleNodeDerivation
-                                    .Derive(0);                          // IIndexNodeDerivation
-            var derivation2 = testKey.Derive(RoleType.ExternalChain, 0); // implicit IIndexNodeDerivation
-            var derivation3 = testKey.DeriveAccount(0)                   // implicit IAccountNodeDerivation
-                                    .Derive(RoleType.ExternalChain)      // implicit IRoleNodeDerivation
-                                    .Derive(0);                          // IIndexNodeDerivation
-
-            // Assert
-            AssertDerivedKeys(paymentPrv1, paymentPub1, derivation);
-            AssertDerivedKeys(paymentPrv1, paymentPub1, derivation2);
-            AssertDerivedKeys(paymentPrv1, paymentPub1, derivation3);
-        }
-
         /// <summary>
         /// Use Case:
         /// I'm writing a light client (similar to Yoroi, Nami, C64Minter, etc). 
@@ -138,63 +111,23 @@ namespace CardanoSharp.Wallet.Test
         /// </summary>
         [Theory]
         [InlineData(__mnemonic)]
-        public void AccountKeyDerivation_AlexVersion(string words)
+        public void AccountKeyDerivation_UsingExtensionMethods(string words)
         {
-            var path = WalletPath.Parse("m/1852'/1815'/0'");
+            // Arrange
+            var accountPath = WalletPath.Parse("m/1852'/1815'/0'");
+            var paymentPath = WalletPath.Parse("0/1");
 
             // User enters in the Mnemonic and Spending Password. 
             var mnemonic = _keyService.Restore(words);
             PrivateKey rootKey = mnemonic.GetRootKey();
-            var accountPrv = rootKey.Derive(path.ToString());
-            var accountPub = accountPrv.GetPublicKey(false);
 
-            // use the Spending Password to 2-Way encrypt the Private Key
-            // and then store both the encrypted Private Key and the plain Public Key.
-            var blob = new Tuple<PrivateKey, PublicKey>(accountPrv.Encrypt("password"), accountPub);
-            var store = JsonSerializer.Serialize(blob);
+            var account = rootKey
+                .Derive() // m
+                .Derive(accountPath.Purpose)
+                .Derive(accountPath.Coin)
+                .Derive(accountPath.AccountIndex);
 
-            // The user wants to generate a random/incremented address.
-            // I take the Public Key and Derive (0/1).
-            var fluentPub = new FluentDerivationPublicKey(accountPub);
-            var payment = fluentPub
-                .Derive(RoleType.ExternalChain)
-                .Derive(1);
-            var paymentPub = payment.PublicKey;
-            var paymentPrv = payment.PrivateKey; // should be null
-
-            // The user now wants to send a transaction.
-            // They enter in the ADDR, Amount to Send, and their Spending Password.
-            // I can now decrypt the Account Private Key and derive down to the "0/1"
-            // Index Private Key. This allows me to now sign the Transaction.
-            var load = JsonSerializer.Deserialize<Tuple<PrivateKey, PublicKey>>(store);
-            var loadedPrv = load.Item1.Decrypt("password");
-
-            var fluentPrv = new FluentDerivationPrivateKey(loadedPrv.Key, loadedPrv.Chaincode);
-            var index = fluentPrv
-                .Derive(RoleType.ExternalChain)
-                .Derive(1);
-
-            // paymentPrv = index.PrivateKey;
-            // var prv = rootKey.Derive("m/1852'/1815'/0'/0/1");
-            // TODO: this should not fail
-            // Assert.Equal(prv.Key, paymentPrv.Key); // Fails
-        }
-
-        [Theory]
-        [InlineData(__mnemonic)]
-        public void AccountKeyDerivation_KyleVersion(string words)
-        {
-            var path = WalletPath.Parse("m/1852'/1815'/0'");
-
-            // User enters in the Mnemonic and Spending Password. 
-            var mnemonic = _keyService.Restore(words);
-            PrivateKey rootKey = mnemonic.GetRootKey();
-            var account = new MasterNodeDerivation(rootKey)
-                .Derive(path.Purpose)
-                .Derive(path.Coin)
-                .Derive(path.AccountIndex);
             account.SetPublicKey();
-
 
             // use the Spending Password to 2-Way encrypt the Private Key
             // and then store both the encrypted Private Key and the plain Public Key.
@@ -203,29 +136,73 @@ namespace CardanoSharp.Wallet.Test
 
             // The user wants to generate a random/incremented address.
             // I take the Public Key and Derive (0/1).
-            var roleNodePub = new RoleNodeDerivation(account.PublicKey, RoleType.ExternalChain);
-            var payment = roleNodePub
-                .Derive(1);
-            var paymentPub = payment.PublicKey;
-            var paymentPrv = payment.PrivateKey; // should be null
+            var payment = account.PublicKey
+                .Derive(paymentPath.Role)
+                .Derive(paymentPath.Index);
 
             // The user now wants to send a transaction.
             // They enter in the ADDR, Amount to Send, and their Spending Password.
-            // I can now decrypt the Account Private Key and derive down to the "0/1"
-            // Index Private Key. This allows me to now sign the Transaction.
+            // I can now decrypt the Account Private Key...
             var load = JsonSerializer.Deserialize<Tuple<PrivateKey, PublicKey>>(store);
             var loadedPrv = load.Item1.Decrypt("password");
 
-            var roleNodePrv = new RoleNodeDerivation(new PrivateKey(loadedPrv.Key, loadedPrv.Chaincode), RoleType.ExternalChain);
-            var index = roleNodePrv
-                .Derive(1);
-            paymentPrv = index.PrivateKey;
+            // ...and derive down to the "0/1" Index Private Key.
+            // This allows me to now sign the Transaction.
+            var index = loadedPrv
+                .Derive(paymentPath.Role)
+                .Derive(paymentPath.Index);
 
+            // Assert
             var prv = rootKey.Derive("m/1852'/1815'/0'/0/1");
-            var pub = prv.GetPublicKey(false);
+            AssertDerivedKeys(prv, prv.GetPublicKey(false), index);
 
-            Assert.Equal(prv.Key, paymentPrv.Key);
-            Assert.Equal(pub.Key, paymentPub.Key);
+            Assert.Null(payment.PrivateKey);
+        }
+
+        [Theory]
+        [InlineData(__mnemonic)]
+        public void AccountKeyDerivation_UsingExplicitNodeDerivations(string words)
+        {
+            var accountPath = WalletPath.Parse("m/1852'/1815'/0'");
+            var paymentPath = WalletPath.Parse("0/1");
+
+            // User enters in the Mnemonic and Spending Password. 
+            var mnemonic = _keyService.Restore(words);
+            PrivateKey rootKey = mnemonic.GetRootKey();
+
+            var account = new MasterNodeDerivation(rootKey)
+                .Derive(accountPath.Purpose)
+                .Derive(accountPath.Coin)
+                .Derive(accountPath.AccountIndex);
+
+            account.SetPublicKey();
+            
+            // use the Spending Password to 2-Way encrypt the Private Key
+            // and then store both the encrypted Private Key and the plain Public Key.
+            var blob = new Tuple<PrivateKey, PublicKey>(account.PrivateKey.Encrypt("password"), account.PublicKey);
+            var store = JsonSerializer.Serialize(blob);
+
+            // The user wants to generate a random/incremented address.
+            // I take the Public Key and Derive (0/1).
+            var roleNodePub = new RoleNodeDerivation(account.PublicKey, paymentPath.Role);
+            var payment = roleNodePub.Derive(paymentPath.Index);
+            
+            // The user now wants to send a transaction.
+            // They enter in the ADDR, Amount to Send, and their Spending Password.
+            // I can now decrypt the Account Private Key...
+            var load = JsonSerializer.Deserialize<Tuple<PrivateKey, PublicKey>>(store);
+            var loadedPrv = load.Item1.Decrypt("password");
+
+            // ...and derive down to the "0/1" Index Private Key.
+            // This allows me to now sign the Transaction.
+            var roleNodePrv = new RoleNodeDerivation(loadedPrv, paymentPath.Role);
+            var index = roleNodePrv.Derive(paymentPath.Index);
+
+            // Assert
+            var prv = rootKey.Derive("m/1852'/1815'/0'/0/1");
+            AssertDerivedKeys(prv, prv.GetPublicKey(false), index);
+
+            Assert.Null(payment.PrivateKey);
         }
 
         private static void AssertDerivedKeys(PrivateKey prv, PublicKey pub, IPathDerivation derivation)
@@ -250,14 +227,14 @@ namespace CardanoSharp.Wallet.Test
             return (privateKey, privateKey.GetPublicKey(false));
         }
 
-        private FluentDerivationPrivateKey getTestRootKey(Mnemonic mnemonic, string password = "")
+        private PrivateKey getTestRootKey(Mnemonic mnemonic, string password = "")
         {
             var rootKey = KeyDerivation.Pbkdf2(password, mnemonic.Entropy, KeyDerivationPrf.HMACSHA512, 4096, 96);
             rootKey[0] &= 248;
             rootKey[31] &= 31;
             rootKey[31] |= 64;
 
-            return new FluentDerivationPrivateKey(rootKey.Slice(0, 64), rootKey.Slice(64));
+            return new PrivateKey(rootKey.Slice(0, 64), rootKey.Slice(64));
         }
 
         #region AccountDiscovery
