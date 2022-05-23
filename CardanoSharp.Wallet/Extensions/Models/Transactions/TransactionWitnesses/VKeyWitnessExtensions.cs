9f914d071022e0059ca024fe02c65d32c68bd442
Paul Cowan <paul.cowan@gmail.com>
2022-05-23 11:10:07 +0200

    Adding validation for vkeywitness GetCBOR

--- CardanoSharp.Wallet/Extensions/Models/Transactions/TransactionWitnesses/VKeyWitnessExtensions.cs
+++ CardanoSharp.Wallet/Extensions/Models/Transactions/TransactionWitnesses/VKeyWitnessExtensions.cs
@@ -12,6 +12,16 @@ namespace CardanoSharp.Wallet.Extensions.Models.Transactions.TransactionWitnesse
     {
         public static CBORObject GetCBOR(this VKeyWitness vKeyWitness, TransactionBody transactionBody, AuxiliaryData auxiliaryData)
         {
+            //validation
+            if (vKeyWitness.VKey.Key.Length != 32)
+            {
+                throw new ArgumentException("vKeyWitness.VKey.Key not expected length (expected 32)");
+            }
+            if (vKeyWitness.SKey != null && vKeyWitness.SKey.Key.Length != 32)
+            {
+                throw new ArgumentException("vKeyWitness.SKey.Key present and not expected length (expected 32)");
+            }
+
             //sign body
             var txBodyHash = HashUtility.Blake2b256(transactionBody.GetCBOR(auxiliaryData).EncodeToBytes());
             vKeyWitness.Signature = vKeyWitness.SKey.Sign(txBodyHash);
