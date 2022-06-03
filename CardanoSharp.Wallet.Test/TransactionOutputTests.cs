caa3f4f9466c11c9e2846d537f53ad8b926ee1f6
Paul Cowan <paul.cowan@gmail.com>
2022-06-03 13:15:08 +0200

    Simplified min lovelace calc tests

--- CardanoSharp.Wallet.Test/TransactionOutputTests.cs
+++ CardanoSharp.Wallet.Test/TransactionOutputTests.cs
@@ -10,6 +10,7 @@ using System.Threading.Tasks;
 using Xunit;
 using CardanoSharp.Wallet.Models.Keys;
 using CardanoSharp.Wallet.Extensions.Models.Transactions;
+using CardanoSharp.Wallet.Models.Transactions;
 
 namespace CardanoSharp.Wallet.Test
 {
@@ -34,49 +35,14 @@ namespace CardanoSharp.Wallet.Test
             return hash;
         }
 
-        private byte[] getGenesisTransaction()
-        {
-            var hash = new byte[32];
-            for (var i = 0; i < hash.Length; i++)
-            {
-                hash[i] = 0x00;
-            }
-            return hash;
-        }
-
-        private PrivateKey getBase15WordWallet()
-        {
-            var words = "art forum devote street sure rather head chuckle guard poverty release quote oak craft enemy";
-            var mnemonic = _keyService.Restore(words);
-            return mnemonic.GetRootKey();
-        }
-
-        private (PrivateKey, PublicKey) getKeyPairFromPath(string path, PrivateKey rootKey)
-        {
-            var privateKey = rootKey.Derive(path);
-            return (privateKey, privateKey.GetPublicKey(false));
-        }
-
         [Fact]
-        public void CalculateMinUtxoTest_MultiOutput_ShouldMatchCoinValue()
+        public void CalculateMinUtxoTest_TestBundles_ShouldMatchValue()
         {
-            var rootKey = getBase15WordWallet();
-
-            //get payment keys
-            (var paymentPrv, var paymentPub) = getKeyPairFromPath("m/1852'/1815'/0'/0/0", rootKey);
-
-            //get change keys
-            (var changePrv, var changePub) = getKeyPairFromPath("m/1852'/1815'/0'/1/0", rootKey);
-
-            //get stake keys
-            (var stakePrv, var stakePub) = getKeyPairFromPath("m/1852'/1815'/0'/2/0", rootKey);
-
-            var baseAddr = _addressService.GetBaseAddress(paymentPub, stakePub, NetworkType.Testnet);
-            var changeAddr = _addressService.GetBaseAddress(changePub, stakePub, NetworkType.Testnet);
-
             var tokenBundle1 = TokenBundleBuilder.Create
                 .AddToken(getGenesisPolicyId(), "00010203".HexToByteArray(), 60)
                 .AddToken(getGenesisPolicyId(), "00010204".HexToByteArray(), 240);
+
+            Assert.Equal((ulong)1413762, tokenBundle1.Build().CalculateMinUtxoLovelace());
 
             var tokenBundle2 = TokenBundleBuilder.Create
                 .AddToken(getGenesisPolicyId(), "00010205".HexToByteArray(), 60)
@@ -84,19 +50,17 @@ namespace CardanoSharp.Wallet.Test
                 .AddToken(getGenesisPolicyId(), "00010207".HexToByteArray(), 180)
                 .AddToken(getGenesisPolicyId(), "00010208".HexToByteArray(), 240);
 
-            var transactionBody = TransactionBodyBuilder.Create
-                .AddInput(getGenesisTransaction(), 0)
-                .AddInput(getGenesisTransaction(), 0)
-                .AddOutput(baseAddr, 1413762, tokenBundle1)
-                .AddOutput(changeAddr, 1551690, tokenBundle2)
-                .SetFee(1)
-                .Build();
+            Assert.Equal((ulong)1551690, tokenBundle2.Build().CalculateMinUtxoLovelace());
+        }
 
-            foreach (var output in transactionBody.TransactionOutputs)
-            {
-                var minValue = output.CalculateMinUtxoLovelace();
-                Assert.Equal(output.Value.Coin, minValue);
-            }
+        [Fact]
+        public void CalculateMinUtxoTest_CoinOnly_ShouldMatchValue()
+        {
+            var txOutValue = new TransactionOutputValue() { Coin = 1234567 };
+            var txOut = new TransactionOutput() { Value = txOutValue };
+
+            var minValue = txOut.CalculateMinUtxoLovelace();
+            Assert.Equal((ulong)999978, minValue);
         }
     }
 }
