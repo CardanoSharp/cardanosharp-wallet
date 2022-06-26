11bb29143761a93ce5affe3a3158c8236bbffdf3
Paul Cowan <paul.cowan@gmail.com>
2022-06-26 21:04:50 +0200

    adding transaction unspent output as per cip30 spec and related extension method

---
+++ CardanoSharp.Wallet/CIPs/CIP30/Models/TransactionUnspentOutput.cs
@@ -0,0 +1,11 @@
+﻿using CardanoSharp.Wallet.Models.Transactions;
+
+namespace CardanoSharp.Wallet.CIPs.CIP30.Models
+{
+	public class TransactionUnspentOutput
+	{
+		public TransactionInput Input { get; set; }
+
+		public TransactionOutput Output { get; set; }
+	}
+}
