d08bac69c31c22c35b1528a56e0dc36ecf5c849c
safestak-keith <keith@safestak.com>
2022-06-29 17:52:49 +1000

    Initial types

---
+++ CardanoSharp.Wallet/CIPs/CIP8/IMessageSigner.cs
@@ -0,0 +1,21 @@
+﻿using CardanoSharp.Wallet.CIPs.CIP8.Models;
+using CardanoSharp.Wallet.Models.Keys;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CardanoSharp.Wallet.CIPs.CIP8
+{
+    public interface IMessageSigner
+    {
+        CoseMessageSignature Sign(
+            byte[] address,
+            byte[] payload,
+            PrivateKey signingKey);
+
+        bool Verify(
+            byte[] address,
+            byte[] payload,
+            PrivateKey signingKey);
+    }
+}
