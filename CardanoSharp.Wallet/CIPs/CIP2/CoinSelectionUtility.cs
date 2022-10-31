674dd28b39c765bc9ab4ac210f7c1bcdb33391c6
Nicholas Maselli <maselli.nicholas@gmail.com>
2022-10-31 18:26:28 -0400

    coin selection utility

--- CardanoSharp.Wallet/CIPs/CIP2/CoinSelectionUtility.cs
+++ CardanoSharp.Wallet/CIPs/CIP2/CoinSelectionUtility.cs
@@ -10,14 +10,14 @@ namespace CardanoSharp.Wallet.CIPs.CIP2
 {
     public static class CoinSelectionUtility
     {
-        public static CoinSelection UseLargestFirst(TransactionBodyBuilder tbb, List<Utxo> utxos, string changeAddress, int limit = 20, ulong feeBuffer = 0)
+        public static CoinSelection UseLargestFirst(this TransactionBodyBuilder tbb, List<Utxo> utxos, string changeAddress, int limit = 20, ulong feeBuffer = 0)
         {
             var cs = new CoinSelectionService(new LargestFirstStrategy(), new MultiTokenBundleStrategy());
             var tb = tbb.Build();
             return cs.GetCoinSelection(tb.TransactionOutputs.ToList(), utxos, changeAddress, limit, feeBuffer);
         }
 
-        public static CoinSelection UseRandomImprove(TransactionBodyBuilder tbb, List<Utxo> utxos, string changeAddress, int limit = 20, ulong feeBuffer = 0)
+        public static CoinSelection UseRandomImprove(this TransactionBodyBuilder tbb, List<Utxo> utxos, string changeAddress, int limit = 20, ulong feeBuffer = 0)
         {
             var cs = new CoinSelectionService(new RandomImproveStrategy(), new MultiTokenBundleStrategy());
             var tb = tbb.Build();
