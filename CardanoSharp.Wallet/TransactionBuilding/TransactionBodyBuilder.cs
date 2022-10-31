60c42aa95db720fd0e426da86f5643ebaa878608
Nicholas Maselli <maselli.nicholas@gmail.com>
2022-10-31 16:00:06 -0400

    remove from change fee

--- CardanoSharp.Wallet/TransactionBuilding/TransactionBodyBuilder.cs
+++ CardanoSharp.Wallet/TransactionBuilding/TransactionBodyBuilder.cs
@@ -170,11 +170,7 @@ namespace CardanoSharp.Wallet.TransactionBuilding
             ulong feePerChangeOutput = fee.Value / (ulong)changeOutputs.Count();
             ulong feeRemaining = fee.Value % (ulong)changeOutputs.Count();
             bool needToApplyRemaining = true;
-            foreach (var o in changeOutputs
-                         .Where(x => x.OutputPurpose == OutputPurpose.Change
-                           && (x.Value.MultiAsset is null 
-                               || (x.Value.MultiAsset is not null 
-                                   && !x.Value.MultiAsset.Any()))))
+            foreach (var o in changeOutputs)
             {
                 if (needToApplyRemaining)
                 {
