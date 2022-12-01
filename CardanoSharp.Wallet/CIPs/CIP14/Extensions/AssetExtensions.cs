f521e1f0699902cbd3a38039e23cf44954957fde
Paul Cowan <paul.cowan@inversion.dev>
2022-12-02 01:31:27 +0200

    using const

--- CardanoSharp.Wallet/CIPs/CIP14/Extensions/AssetExtensions.cs
+++ CardanoSharp.Wallet/CIPs/CIP14/Extensions/AssetExtensions.cs
@@ -8,11 +8,13 @@ namespace CardanoSharp.Wallet.CIPs.CIP14.Extensions
 {
     public static class AssetExtensions
     {
+        public const string FingerprintHrp = "asset";
+
         public static string GetFingerprint(this Asset asset)
         {
             var tokenTypeId = $"{asset.PolicyId}{asset.Name}";
             var hashed = HashUtility.Blake2b160(tokenTypeId.HexToByteArray());
-            return Bech32.Encode(hashed, "asset");
+            return Bech32.Encode(hashed, FingerprintHrp);
         }
 
         public static Asset ToAsset(this string tokenTypeId, long quantity = 0)
@@ -29,7 +31,7 @@ namespace CardanoSharp.Wallet.CIPs.CIP14.Extensions
 
         public static string GetFingerprint(this string tokenTypeId)
         {
-            return Bech32.Encode(HashUtility.Blake2b160(tokenTypeId.HexToByteArray()), "asset");
+            return Bech32.Encode(HashUtility.Blake2b160(tokenTypeId.HexToByteArray()), FingerprintHrp);
         }
     }
 }
