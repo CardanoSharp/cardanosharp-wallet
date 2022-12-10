9a7a9570a598ea08e94bbe69345129a5b6f7bcf9
Nicholas Maselli <maselli.nicholas@gmail.com>
2022-12-10 09:21:34 -0500

    making progress on plutus data

--- CardanoSharp.Wallet/Extensions/Models/DatumOptionExtension.cs
+++ CardanoSharp.Wallet/Extensions/Models/DatumOptionExtension.cs
@@ -12,16 +12,55 @@ namespace CardanoSharp.Wallet.Extensions.Models
             var cborDatum = CBORObject.NewArray();
 
             //datum_option = [ 0, $hash32 // 1, data ]
-            if (datumOption.Hash is not null) {
+            if (datumOption.Hash is not null)
+            {
                 cborDatum.Add(0);
                 cborDatum.Add(CBORObject.DecodeFromBytes(datumOption.Hash));
             }
-            else if (datumOption.Data is not null) {
+            else if (datumOption.Data is not null)
+            {
                 cborDatum.Add(1);
-                cborDatum.Add(CBORObject.FromObject(datumOption.Data.GetCBOR().EncodeToBytes()).WithTag(24));                
+                cborDatum.Add(
+                    CBORObject.FromObject(datumOption.Data.GetCBOR().EncodeToBytes()).WithTag(24)
+                );
             }
-            
+
             return cborDatum;
+        }
+
+        public static DatumOption GetDatumOption(this CBORObject datumOptionCbor)
+        {
+            if (datumOptionCbor == null)
+            {
+                throw new ArgumentNullException(nameof(datumOptionCbor));
+            }
+
+            if (datumOptionCbor.Type != CBORType.Array)
+            {
+                throw new ArgumentException("datumOptionCbor is not expected type CBORType.Array");
+            }
+
+            if (datumOptionCbor.Count != 2)
+            {
+                throw new ArgumentException(
+                    "datumOptionCbor has unexpected number of elements (expected 2)"
+                );
+            }
+
+            DatumOption datumOption = new DatumOption();
+            var datumType = datumOptionCbor[0].DecodeValueToInt32();
+            if (datumType == 0)
+            {
+                datumOption.Hash = (byte[])datumOptionCbor[1].DecodeValueByCborType();
+            }
+            else if (datumType == 1)
+            {
+                var dataCbor = datumOptionCbor[1].Untag();
+                var dataByteArray = ((string)dataCbor.DecodeValueByCborType()).HexToByteArray();
+                datumOption.Data = new PlutusDataBytes { Value = dataByteArray };
+            }
+
+            return datumOption;
         }
 
         public static byte[] Serialize(this Redeemer redeemer)
@@ -29,9 +68,9 @@ namespace CardanoSharp.Wallet.Extensions.Models
             return redeemer.GetCBOR().EncodeToBytes();
         }
 
-        public static Redeemer Deserialize(this byte[] bytes)
+        public static DatumOption Deserialize(this byte[] bytes)
         {
-            return CBORObject.DecodeFromBytes(bytes).GetRedeemer();
+            return CBORObject.DecodeFromBytes(bytes).GetDatumOption();
         }
     }
 }
