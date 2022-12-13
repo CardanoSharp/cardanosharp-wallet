457c694245f1a95aded66491ea06dfcd871baa9f
Paul Cowan <paul.cowan@inversion.dev>
2022-12-13 20:54:13 +0200

    adding address tests for known bech32 to hex conversions

--- CardanoSharp.Wallet.Test/AddressTests.cs
+++ CardanoSharp.Wallet.Test/AddressTests.cs
@@ -1,20 +1,19 @@
 ﻿using CardanoSharp.Wallet.Encoding;
 using CardanoSharp.Wallet.Enums;
-using CardanoSharp.Wallet.Models.Addresses;
-using CardanoSharp.Wallet.Extensions.Models;
-using Xunit;
-using CardanoSharp.Wallet.Models.Keys;
-using System;
 using CardanoSharp.Wallet.Extensions;
-using CardanoSharp.Wallet.TransactionBuilding;
+using CardanoSharp.Wallet.Extensions.Models;
+using CardanoSharp.Wallet.Models.Addresses;
+using CardanoSharp.Wallet.Models.Keys;
 using CardanoSharp.Wallet.Utilities;
+using System;
+using Xunit;
 
 namespace CardanoSharp.Wallet.Test
 {
     public class AddressTests
     {
         private readonly IMnemonicService _keyService;
-        const string __mnemonic = "art forum devote street sure rather head chuckle guard poverty release quote oak craft enemy";
+        private const string __mnemonic = "art forum devote street sure rather head chuckle guard poverty release quote oak craft enemy";
 
         public AddressTests()
         {
@@ -22,7 +21,7 @@ namespace CardanoSharp.Wallet.Test
         }
 
         [Theory]
-        //Delegation Addresses    
+        //Delegation Addresses
         [InlineData("addr_test", "addr_test1qz2fxv2umyhttkxyxp8x0dlpdt3k6cwng5pxj3jhsydzer3jcu5d8ps7zex2k2xt3uqxgjqnnj83ws8lhrn648jjxtwq2ytjqp")]
         [InlineData("addr", "addr1qx2fxv2umyhttkxyxp8x0dlpdt3k6cwng5pxj3jhsydzer3jcu5d8ps7zex2k2xt3uqxgjqnnj83ws8lhrn648jjxtwqfjkjv7")]
         [InlineData("addr_test", "addr_test1qpu5vlrf4xkxv2qpwngf6cjhtw542ayty80v8dyr49rf5ewvxwdrt70qlcpeeagscasafhffqsxy36t90ldv06wqrk2qum8x5w")]
@@ -47,7 +46,7 @@ namespace CardanoSharp.Wallet.Test
             Assert.Equal(addr, addr2);
         }
 
-        //28 
+        //28
         //addr1q8d9pcrn38veygv638ftw0f82gm4h6rmrs599pkr3qfxx073eyjrmj0hnx6xz8emx03l6hszjclm8fmnlaewe4adp7dqsd8pa6
         //28
         [Theory]
@@ -63,7 +62,6 @@ namespace CardanoSharp.Wallet.Test
             Assert.Equal(prefix, address.Prefix);
             Assert.Equal(prefix == "addr" ? NetworkType.Mainnet : NetworkType.Testnet, address.NetworkType);
             Assert.Equal(AddressType.Base, address.AddressType);
-
         }
 
         /// <summary>
@@ -72,10 +70,10 @@ namespace CardanoSharp.Wallet.Test
         /// that addresses consist of "header part", "payment address part" and "reward address part"
         /// that "payment address part" for different paths (CIP1852) differ
         /// that "reward address part" for different paths are equal
-        /// 
-        /// inspired by Andrew Westberg (NerdOut) Addresses Video 
+        ///
+        /// inspired by Andrew Westberg (NerdOut) Addresses Video
         /// https://www.youtube.com/watch?v=NjPf_b9UQNs&t=396)
-        /// 
+        ///
         /// 0 0 79467c69a9ac66280174d09d62575ba955748b21dec3b483a9469a65 cc339a35f9e0fe039cf510c761d4dd29040c48e9657fdac7e9c01d94
         /// 0 0 1fd57d18565e3a17cd194f190d349c2b7309eaf70f3f3bf884b0eada cc339a35f9e0fe039cf510c761d4dd29040c48e9657fdac7e9c01d94
         /// 0 0 f36b29ceede650f850ee705a3a89ec041e24397d1a0d803d6af7e3f2 cc339a35f9e0fe039cf510c761d4dd29040c48e9657fdac7e9c01d94
@@ -92,7 +90,7 @@ namespace CardanoSharp.Wallet.Test
             //arrange
             var mnemonic = _keyService.Restore(words);
             var rootKey = mnemonic.GetRootKey();
-            
+
             ////get payment keys
             (var paymentPrv1, var paymentPub1) = getKeyPairFromPath("m/1852'/1815'/0'/0/0", rootKey);
             (var paymentPrv2, var paymentPub2) = getKeyPairFromPath("m/1852'/1815'/0'/0/1", rootKey);
@@ -100,16 +98,16 @@ namespace CardanoSharp.Wallet.Test
 
             ////get stake keys
             (var stakePrv, var stakePub) = getKeyPairFromPath("m/1852'/1815'/0'/2/0", rootKey);
-            
+
             var baseAddr1 = AddressUtility.GetBaseAddress(paymentPub1, stakePub, NetworkType.Testnet);
             var baseAddr2 = AddressUtility.GetBaseAddress(paymentPub2, stakePub, NetworkType.Testnet);
             var baseAddr3 = AddressUtility.GetBaseAddress(paymentPub3, stakePub, NetworkType.Testnet);
 
             //act
-            var hex1 = baseAddr1.ToStringHex(); 
+            var hex1 = baseAddr1.ToStringHex();
             var hex2 = baseAddr2.ToStringHex();
-            var hex3 = baseAddr3.ToStringHex(); 
-                                                
+            var hex3 = baseAddr3.ToStringHex();
+
             // assert
             Assert.EndsWith(stakingAddr, hex1);
             Assert.EndsWith(stakingAddr, hex2);
@@ -205,6 +203,23 @@ namespace CardanoSharp.Wallet.Test
             var address = addr.ToAddress();
             var stakeKeyHash = address.GetStakeKeyHash();
             Assert.Equal(skh.HexToByteArray(), stakeKeyHash);
+        }
+
+        [Theory]
+        [InlineData("stake_test1uqhtd5fclk3ljpa6hfxnuyxacr833epg0jzgwkvyryyu7qg0vxxhc", "2eb6d138fda3f907baba4d3e10ddc0cf18e4287c848759841909cf01")]
+        [InlineData("stake_test1upkd63mghdftntlqfwhxkc4shn5auy72nn6wyrpdp7ksx4qg72kne", "6cdd4768bb52b9afe04bae6b62b0bce9de13ca9cf4e20c2d0fad0354")]
+        [InlineData("stake_test1ur22cmnvu55kypy049jjc8uzyhqdj6lswvp3svgvlpusvcsx2nctx", "d4ac6e6ce52962048fa9652c1f8225c0d96bf0730318310cf8790662")]
+        [InlineData("stake_test1uqyt95qs2qyhr0xc0gg4vmzt24yyw27kta682a4duhd3eaql492l0", "08b2d010500971bcd87a11566c4b5548472bd65f747576ade5db1cf4")]
+        [InlineData("addr_test1qzxla3xpk8hmuw5jmspjanq2rmglhn3zxf9f0kpmrjn4jfewkmgn3ldrlyrm4wjd8cgdmsx0rrjzslyysavcgxgfeuqsj6e03d", "008dfec4c1b1efbe3a92dc032ecc0a1ed1fbce22324a97d83b1ca759272eb6d138fda3f907baba4d3e10ddc0cf18e4287c848759841909cf01")]
+        [InlineData("addr_test1qrvtessdca4qhgkg8tc06ahq04u9qd3xgh8lwus4h64978tvm4rk3w6jhxh7qjawdd3tp08fmcfu4885ugxz6radqd2qveyl8e", "00d8bcc20dc76a0ba2c83af0fd76e07d7850362645cff77215beaa5f1d6cdd4768bb52b9afe04bae6b62b0bce9de13ca9cf4e20c2d0fad0354")]
+        [InlineData("addr_test1qqlckcsaynvmt5aqyx6fwng472atyq8wjyykskxafypk3nw543hxeeffvgzgl2t99s0cyfwqm94lqucrrqcse7reqe3q57q70d", "003f8b621d24d9b5d3a021b4974d15f2bab200ee91096858dd490368cdd4ac6e6ce52962048fa9652c1f8225c0d96bf0730318310cf8790662")]
+        [InlineData("addr_test1qpknz5uj64vmh4863ek7j3vkqj9wpusmyel06uxd2h95rlqgktgpq5qfwx7ds7s32ekyk42ggu4avhm5w4m2mewmrn6qq6wgan", "006d315392d559bbd4fa8e6de94596048ae0f21b267efd70cd55cb41fc08b2d010500971bcd87a11566c4b5548472bd65f747576ade5db1cf4")]
+        public void MatchAddressBytesToKnownHexValuesTest(string addr, string knownHexValue)
+        {
+            var address = addr.ToAddress();
+            var actualHexValue = address.ToStringHex().ToLower();
+            knownHexValue = knownHexValue.ToLower();
+            Assert.Equal(actualHexValue, knownHexValue);
         }
     }
 }
