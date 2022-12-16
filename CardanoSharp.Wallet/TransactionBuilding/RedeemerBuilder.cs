adb759ef15ebf8d1c7740286a6da5766704cecc8
Nicholas Maselli <maselli.nicholas@gmail.com>
2022-12-15 23:40:04 -0500

    transaction extensions for utxo and adding redeemer count

--- CardanoSharp.Wallet/TransactionBuilding/RedeemerBuilder.cs
+++ CardanoSharp.Wallet/TransactionBuilding/RedeemerBuilder.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
 using CardanoSharp.Wallet.Enums;
+using CardanoSharp.Wallet.Extensions;
+using CardanoSharp.Wallet.Models;
 using CardanoSharp.Wallet.Models.Transactions;
 using CardanoSharp.Wallet.Models.Transactions.TransactionWitness.PlutusScripts;
 using CardanoSharp.Wallet.TransactionBuilding;
@@ -12,6 +14,7 @@ namespace CardanoSharp.Wallet.TransactionBuilding
         IRedeemerBuilder SetTag(RedeemerTag redeemerTag);
         IRedeemerBuilder SetIndex(uint index);
         IRedeemerBuilder SetIndex(Transaction transaction, TransactionInput scriptTransactionInput);
+        IRedeemerBuilder SetIndex(Transaction transaction, Utxo utxo);
         IRedeemerBuilder SetPlutusData(IPlutusData plutusData);
         IRedeemerBuilder SetExUnits(ExUnits exUnits);
     }
@@ -57,6 +60,21 @@ namespace CardanoSharp.Wallet.TransactionBuilding
             transactionInputs.Sort((a, b) => $"{a.TransactionId}#{a.TransactionIndex}".CompareTo($"{b.TransactionId}#{b.TransactionIndex}"));
 
             uint index = (uint)transactionInputs.IndexOf(scriptTransactionInput);
+            _model.Index = index;
+            return this;
+        }
+
+        public IRedeemerBuilder SetIndex(Transaction transaction, Utxo utxo)
+        {
+            List<TransactionInput> transactionInputs = new List<TransactionInput>();
+            transactionInputs.AddRange((List<TransactionInput>)transaction.TransactionBody.TransactionInputs);
+
+            //https://github.com/bloxbean/cardano-client-lib/blob/7322b16030d8fa3ac5417d5dc58c92df401855ad/function/src/main/java/com/bloxbean/cardano/client/function/helper/RedeemerUtil.java
+            //https://cardano.stackexchange.com/questions/7969/meaning-of-index-of-redeemer-in-serialization-lib-10-4
+            // Sort transaction inputs to determine redeemer index
+            transactionInputs.Sort((a, b) => $"{a.TransactionId}#{a.TransactionIndex}".CompareTo($"{b.TransactionId}#{b.TransactionIndex}"));
+
+            uint index = (uint)transactionInputs.FindIndex(t => t.TransactionId.ToStringHex() == utxo.TxHash && t.TransactionIndex == utxo.TxIndex);
             _model.Index = index;
             return this;
         }
