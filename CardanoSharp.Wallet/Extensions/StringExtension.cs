65e101a11de13608db0dfc432f2266174ba1c35e
Paul Cowan <paul.cowan@inversion.dev>
2023-03-14 00:33:15 +0200

    changing get utxo extension method to use hex string for asset name conversion

--- CardanoSharp.Wallet/Extensions/StringExtension.cs
+++ CardanoSharp.Wallet/Extensions/StringExtension.cs
@@ -14,11 +14,6 @@ namespace CardanoSharp.Wallet.Extensions
 			return hex;
 		}
 
-		public static string GetString(this byte[] bytes)
-		{
-			return System.Text.Encoding.UTF8.GetString(bytes);
-		}
-
 		public static byte[] ToBytes(this string value)
 		{
 			return System.Text.Encoding.UTF8.GetBytes(value);
