9e64f91a2f1d88ff9789f350b0b9c4d95756c056
Nicholas Maselli <maselli.nicholas@gmail.com>
2023-04-16 15:44:39 -0400

    adding back to string utf8

--- CardanoSharp.Wallet/Extensions/StringExtension.cs
+++ CardanoSharp.Wallet/Extensions/StringExtension.cs
@@ -2,21 +2,23 @@
 
 namespace CardanoSharp.Wallet.Extensions
 {
-	public static class StringExtension
-	{
-		public static string ToStringHex(this byte[] bytes)
-		{
-			var hex = BitConverter
-				.ToString(bytes)
-				.Replace("-", "")
-				.ToLower();
+    public static class StringExtension
+    {
+        public static string ToStringHex(this byte[] bytes)
+        {
+            var hex = BitConverter.ToString(bytes).Replace("-", "").ToLower();
 
-			return hex;
-		}
+            return hex;
+        }
 
-		public static byte[] ToBytes(this string value)
-		{
-			return System.Text.Encoding.UTF8.GetBytes(value);
-		}
-	}
+        public static string ToStringUTF8(this byte[] bytes)
+        {
+            return System.Text.Encoding.UTF8.GetString(bytes);
+        }
+
+        public static byte[] ToBytes(this string value)
+        {
+            return System.Text.Encoding.UTF8.GetBytes(value);
+        }
+    }
 }
